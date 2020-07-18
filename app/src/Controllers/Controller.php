3d8ab8581b9ebd7ed6eb1738aa48107a1830f6ae
xlordt <xlordt@gmail.com>
2020-07-17 22:37:03 -0400

    First commit. Project Structure. Initialized composer.

---
+++ app/src/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Class Controller
+ */
+class Controller
+{
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ app/src/Models/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * Class Model
+ */
+class Model
+{
+    /**
+     * Model constructor.
+     */
+    public function __construct()
+    {
+    }
+}
