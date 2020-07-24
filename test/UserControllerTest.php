aa02744ae209cc8fc3098255e01389f693c4a1e9
xlordt <xlordt@gmail.com>
2020-07-24 03:29:17 -0400

    PHPUnit UserTest phase 1.

---
+++ test/UserControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UserControllerTest
+ * @package App\test
+ */
+class UserControllerTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testGetById()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @param $user
+     */
+    public function testCreateUser($user)
+    {
+    }
+
+    /**
+     * @param $id
+     * @param $userContent
+     */
+    public function testUpdateUser($id, $userContent)
+    {
+    }
+}
