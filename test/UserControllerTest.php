f91b29510e4faf3aa25e8e528ef38538588117e2
xlordt <xlordt@gmail.com>
2020-07-24 04:03:36 -0400

    PHPUnit UserTest phase 1.

--- test/UserControllerTest.php
+++ test/UserControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\test;
 
+use App\Controllers\UserController;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -10,26 +11,40 @@ use PHPUnit\Framework\TestCase;
  */
 class UserControllerTest extends TestCase
 {
-    /**
-     *
-     */
+    public function testIndex()
+    {
+        $users = (new UserController())->index();
+        $this->assertIsArray($users);
+
+        foreach ($users as $user) {
+
+            $this->assertObjectHasAttribute('firstName', $user);
+            $this->assertObjectHasAttribute('lastName', $user);
+            $this->assertObjectHasAttribute('email', $user);
+            $this->assertObjectHasAttribute('avatar', $user);
+            $this->assertObjectHasAttribute('password', $user);
+
+            //Check for values.
+            $this->assertTrue(!!$user->getFirstName());
+            $this->assertTrue(!!$user->getLastName());
+            $this->assertTrue(!!$user->getEmail());
+            $this->assertTrue(!!$user->getPassword());
+
+        }
+    }
+
     public function testGetById()
     {
         $this->assertTrue(true);
     }
 
-    /**
-     * @param $user
-     */
-    public function testCreateUser($user)
+    public function testCreateUser()
     {
+        $this->assertTrue(true);
     }
 
-    /**
-     * @param $id
-     * @param $userContent
-     */
-    public function testUpdateUser($id, $userContent)
+    public function testUpdateUser()
     {
+        $this->assertTrue(true);
     }
 }
